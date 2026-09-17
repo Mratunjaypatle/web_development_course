48f01dc3f0ccc80362e45b1c9a0507a4e2b5602e
Mratunjay Patle <mratunjaypatle05@gmail.com>
2026-09-17 20:07:10 +0530

    javaScript

---
+++ JavaScript/first.js
@@ -0,0 +1,30 @@
+// printing something..
+// variable , data type 
+// let var const 
+let name = "Amit Sharma"  // string 
+var city = "Mumbai"
+const salary = 50000   // integer 
+let gender = 'M'        // character
+let a =    [10,20,30,40 , "apple" , true]  // array
+a[4] = 50
+//indexing   0  1  2  3
+let isPresent = true // boolean
+
+// key anond value 
+let obj = {
+    name : "Shruti" , 
+    domain : "Sales Employee",
+    city : "Chennnai"
+}
+obj.gender = 'F'
+
+
+
+console.log(a[0]);
+console.log(obj.name);
+console.log(obj.domain);
+console.log(obj.gender);
+
+
+
+// JavaScript is sesitive langauage 
